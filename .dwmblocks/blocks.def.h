b8a5c4e0b8887e1cc97641b1b0fe2b1b3959f109
alm <nurm.almir@gmail.com>
2022-09-04 22:41:01 +0300

    ADD: my .dwmblocks initial config

---
+++ .dwmblocks/blocks.def.h
@@ -0,0 +1,12 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"  ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"", "sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp",         30,		0},
+	{"BAT:", "acpi | awk '{print $4}' | sed s/,//",			        30,		0},
+	{"", "date '+%b %d (%a) %I:%M%p'",					30,		0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
