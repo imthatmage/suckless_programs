e4013e8224b947d52a8cfbaa5d3c9c8dcf32a009
alm <nurm.almir@gmail.com>
2022-09-04 22:40:34 +0300

    ADD: my .dwm main initial config

---
+++ .dwm_main/autostart.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+nitrogen --restore
+picom -b
+setxkbmap us,ru -option 'grp:alt_shift_toggle'
+xrandr --output DP1 --mode 2560x1440 --right-of eDP1
+#nm-applet
+
+#
+#ram_mem() {
+#  ram_mem=`free -h | awk '(NR==2){ print $4 }'`
+#
+#  echo -e " $ram_mem"
+#}
+#
+#temp() {
+#  temp=`sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp`
+#
+#  echo -e "$temp"
+#}
+#
+#
+#battery() {
+#  battery=`acpi | awk '{print $4}' | sed s/,//`
+#
+#  echo -e "$battery"
+#}
+#
+#
+#dte() {
+#  dte=$(date '+%d.%m.%Y %H:%M')
+#
+#  echo -e " $dte"
+#}
+#
+#
+#while :; do
+#  xsetroot -name "$(temp) | BAT: $(battery) | $(ram_mem) | $(dte)"
+#  # update time every minute
+#  sleep 20s
+#done
+
