96f3ba9609c27f766c4063d3f1f8f389da7ae2aa
imthatmage <nurm.almir@gmail.com>
2022-09-24 23:01:11 +0300

    NEW: dwmblocks script now in separate direction

--- .dwmblocks/blocks.def.h
+++ .dwmblocks/blocks.def.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"  ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	10,		0},
-	{"", "sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp",         5,		0},
-	{"BAT:", "acpi | awk '{print $4}' | sed s/,//",			        30,		0},
-	{"", "date '+%b %d (%a) %I:%M%p'",					30,		0},
+	{" ", "/home/alm/.dwm/.dwmblocks/scripts/ram",	20, 0},
+	{"", "/home/alm/.dwm/.dwmblocks/scripts/temp", 5, 0},
+	{"BAT:", "/home/alm/.dwm/.dwmblocks/scripts/bat", 30, 0},
+	{"", "/home/alm/.dwm/.dwmblocks/scripts/date", 60, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
