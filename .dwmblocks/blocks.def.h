6ea4430aab20c1d54ae86361f9d559095353f4a2
imthatmage <nurm.almir@gmail.com>
2022-09-28 20:46:15 +0300

    ENH: temp status commented

--- .dwmblocks/blocks.def.h
+++ .dwmblocks/blocks.def.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{" ", "/home/alm/.dwm/.dwmblocks/scripts/ram",	20, 0},
-	{"", "/home/alm/.dwm/.dwmblocks/scripts/temp", 5, 0},
+	//{"", "/home/alm/.dwm/.dwmblocks/scripts/temp", 5, 0},
 	{"BAT:", "/home/alm/.dwm/.dwmblocks/scripts/bat", 30, 0},
 	{"", "/home/alm/.dwm/.dwmblocks/scripts/date", 60, 0},
 };
