c3269571a5bf4830dd20dc54db57b27487a35595
imthatmage <nurm.almir@gmail.com>
2022-10-08 14:40:02 +0300

    ENH: Trackpad sensitivity initialisation added to autostart.sh

--- .dwm_main/autostart.sh
+++ .dwm_main/autostart.sh
@@ -2,6 +2,7 @@
 nitrogen --restore
 picom -b 
 setxkbmap us,ru -option 'grp:alt_shift_toggle'
+xinput set-prop "TPPS/2 IBM TrackPoint" 'libinput Accel Speed' -0.5
 xrandr --output DP1 --mode 2560x1440 --right-of eDP1
 dwmblocks
 #nm-applet
