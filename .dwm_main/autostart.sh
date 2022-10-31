ff6bd0953efdc0b31fd33af38cd4143b511cad0d
Almir <nurm.almir@gmail.com>
2022-10-31 16:02:34 +0300

    ADD: some new commands added

--- .dwm_main/autostart.sh
+++ .dwm_main/autostart.sh
@@ -2,8 +2,9 @@
 nitrogen --restore
 picom -b 
 setxkbmap us,ru -option 'grp:alt_shift_toggle'
-xinput set-prop "TPPS/2 IBM TrackPoint" 'libinput Accel Speed' -0.5
-xrandr --output DP1 --mode 2560x1440 --right-of eDP1
+xinput set-prop "TPPS/2 IBM TrackPoint" 'libinput Accel Speed' -0.25
+#xinput set-prop "Logitech USB Receiver" 'libinput Accel Speed' -0.5
+xrandr --output DP1 --mode 2560x1440 --right-of LVDS1 
 dwmblocks
 #nm-applet
 
