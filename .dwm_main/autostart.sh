2dc519c7c61bc491b07a55cab12d3ceb8c4e3b2d
Almir <nurm.almir@gmail.com>
2022-11-04 21:02:49 +0300

    DEL: .dwm_main/autostart_sh: deleted old panel update

--- .dwm_main/autostart.sh
+++ .dwm_main/autostart.sh
@@ -7,38 +7,3 @@ xinput set-prop "TPPS/2 IBM TrackPoint" 'libinput Accel Speed' -0.25
 xrandr --output DP1 --mode 2560x1440 --right-of LVDS1 
 dwmblocks
 #nm-applet
-
-#
-#ram_mem() {
-#  ram_mem=`free -h | awk '(NR==2){ print $4 }'`
-#
-#  echo -e " $ram_mem"
-#}
-#
-#temp() {
-#  temp=`sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp`
-#
-#  echo -e "$temp"
-#}
-#
-#
-#battery() {
-#  battery=`acpi | awk '{print $4}' | sed s/,//`
-#
-#  echo -e "$battery"
-#}
-#
-#
-#dte() {
-#  dte=$(date '+%d.%m.%Y %H:%M')
-#
-#  echo -e " $dte"
-#}
-#
-#
-#while :; do
-#  xsetroot -name "$(temp) | BAT: $(battery) | $(ram_mem) | $(dte)"
-#  # update time every minute
-#  sleep 20s
-#done
-
