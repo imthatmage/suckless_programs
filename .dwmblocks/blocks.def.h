7395591a102b2095df00b56c66fb76dc5ed36e9a
Almir <nurm.almir@gmail.com>
2022-11-07 15:43:32 +0300

    ENH: more complex battery status

--- .dwmblocks/blocks.def.h
+++ .dwmblocks/blocks.def.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{" ", "/home/alm/.dwm/.dwmblocks/scripts/ram",	20, 0},
 	{"", "/home/alm/.dwm/.dwmblocks/scripts/temp", 5, 0},
-	{"BAT:", "/home/alm/.dwm/.dwmblocks/scripts/bat", 30, 0},
+	{"", "/home/alm/.dwm/.dwmblocks/scripts/bat", 30, 0},
 	{"", "/home/alm/.dwm/.dwmblocks/scripts/volume", 0, 2},
 	{"", "/home/alm/.dwm/.dwmblocks/scripts/brightness", 0, 3},
 	{"", "/home/alm/.dwm/.dwmblocks/scripts/date", 60, 0},
