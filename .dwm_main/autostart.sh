1b85dcb8edcf888a3b71be0302057296aba4e010
imthatmage <nurm.almir@gmail.com>
2023-01-01 21:16:30 +0300

    UPD: new sense of trackpoint

--- .dwm_main/autostart.sh
+++ .dwm_main/autostart.sh
@@ -2,7 +2,7 @@
 nitrogen --restore
 picom -b 
 setxkbmap us,ru -option 'grp:alt_shift_toggle'
-xinput set-prop "TPPS/2 IBM TrackPoint" 'libinput Accel Speed' -0.25
+xinput set-prop "TPPS/2 IBM TrackPoint" 'libinput Accel Speed' -0.5
 #xinput set-prop "Logitech USB Receiver" 'libinput Accel Speed' -0.5
 xrandr --output DP1 --mode 2560x1440 --right-of LVDS1 
 dwmblocks
