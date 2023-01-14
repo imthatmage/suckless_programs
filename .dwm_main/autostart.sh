c2197e30679e9da8baef4d9c57e22d4b50ef6ea5
imthatmage <nurm.almir@gmail.com>
2023-01-14 21:05:45 +0300

    FIX: yoga settings

--- .dwm_main/autostart.sh
+++ .dwm_main/autostart.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 nitrogen --restore
+
 picom -b 
+
 setxkbmap us,ru -option 'grp:alt_shift_toggle'
-xinput set-prop "TPPS/2 IBM TrackPoint" 'libinput Accel Speed' -0.5
+
+light -S 40
+
+pamixer --set-volume 20
+
+xinput set-prop "TPPS/2 Elan TrackPoint" 'libinput Accel Speed' -0.5
 #xinput set-prop "Logitech USB Receiver" 'libinput Accel Speed' -0.5
-xrandr --output DP1 --mode 2560x1440 --right-of LVDS1 
+
+xrandr --output HDMI1 --mode 2560x1440 --right-of eDP1
+
 dwmblocks
+
+[[ -f ~/.Xresources ]] && xrdb -merge -I$HOME ~/.Xresources
 #nm-applet
