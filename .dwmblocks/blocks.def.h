c9e3911f4887c41a30b445e089aec90b8beaa0ec
Almir <nurm.almir@gmail.com>
2023-03-13 16:13:36 +0300

    ENH: path to dwm changed

--- .dwmblocks/blocks.def.h
+++ .dwmblocks/blocks.def.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "$HOME/.dwm/.dwmblocks/scripts/ram",	20, 0},
-	{"", "$HOME/.dwm/.dwmblocks/scripts/temp", 5, 0},
-	{"", "$HOME/.dwm/.dwmblocks/scripts/bat", 30, 0},
-	{"", "$HOME/.dwm/.dwmblocks/scripts/volume", 0, 2},
-	{"", "$HOME/.dwm/.dwmblocks/scripts/brightness", 0, 3},
-	{"", "$HOME/.dwm/.dwmblocks/scripts/date", 60, 0},
+	{" ", "$HOME/suckless_programs/.dwmblocks/scripts/ram",	20, 0},
+	{"", "$HOME/suckless_programs/.dwmblocks/scripts/temp", 5, 0},
+	{"", "$HOME/suckless_programs/.dwmblocks/scripts/bat", 30, 0},
+	{"", "$HOME/suckless_programs/.dwmblocks/scripts/volume", 0, 2},
+	{"", "$HOME/suckless_programs/.dwmblocks/scripts/brightness", 0, 3},
+	{"", "$HOME/suckless_programs/.dwmblocks/scripts/date", 60, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
