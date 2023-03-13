d5222ddfb3a02c80f8d29c484bf10b620199e124
Almir <nurm.almir@gmail.com>
2023-03-13 17:51:18 +0300

    ENH: Iosevka as default font

--- .st/config.def.h
+++ .st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SauceCodePro NF:pixelsize=22:antialias=true:autohint=true";
+static char *font = "Iosevka NF:pixelsize=22:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
