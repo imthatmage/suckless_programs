6665fb8b3df82f17d629316c451bc01ff0cf8f0b
Almir <nurm.almir@gmail.com>
2023-03-21 00:26:49 +0300

    ENH: i3lock added

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -5,17 +5,18 @@ picom -b
 
 setxkbmap us,ru -option 'grp:alt_shift_toggle'
 
-light -S 80
+lux -S 50
 
 pamixer --set-volume 20
 
-xinput set-prop "TPPS/2 IBM TrackPoint" 'libinput Accel Speed' -0.5
+xinput set-prop "TPPS/2 Elan TrackPoint" 'libinput Accel Speed' -0.5
 #xinput set-prop "Logitech USB Receiver" 'libinput Accel Speed' -0.5
 
 # xrandr --output HDMI1 --mode 2560x1440 --right-of eDP1
 
-dwmblocks
+xautolock -detectsleep -time 3 -locker "i3lock -c 000000" &
 
+dwmblocks
 # [[ -f ~/.Xresources ]] && xrdb -merge -I$HOME ~/.Xresources
 #nm-applet
 
